96400ca9fe30de91fb3e2f09995f50c654595cb9
Alberto <info@androiddesdecero.com>
2019-08-21 16:21:52 +0200

    express

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.get('/api/cars/list',(req, res)=>{
+    res.send(['BMW S1', 'AUDI A3', 'Mercedes Clase A'])
+})
  
-app.listen(3003)
+app.listen(3003, ()=> console.log('Escuchando Puerto 3003'))
