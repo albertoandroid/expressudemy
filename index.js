82bae84fd310deca49d14a1552401afc1d1b42b1
Alberto <info@androiddesdecero.com>
2019-08-24 16:48:05 +0200

    fin

--- index.js
+++ index.js
@@ -1,32 +1,7 @@
 const express = require('express')
 const app = express()
-
 const car = require('./routes/car')
 app.use(express.json())
 app.use('/api/cars/', car)
-
 const port = process.env.PORT || 3003
-const date = require('./date')
-const morgan = require('morgan')
-
-
-
-app.use(morgan('tiny'))
-
-app.use(date)
-
-app.use('/api/cars/list', function(req, res, next){
-    console.log('Request Type: ', req.method)
-    next()
-})
-
-app.use('/api/loggin', function(req, res, next){
-    console.log('loggin')
-    next()
-})
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
 app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
